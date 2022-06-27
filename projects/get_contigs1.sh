a271768145c76ede1b6c7261ef59c12d97bfa7ff
Kateryna Pantiukh <pantiukh@gmail.com>
2022-06-27 14:53:11 +0300

    Update get_contigs1.sh

--- projects/get_contigs1.sh
+++ projects/get_contigs1.sh
@@ -11,18 +11,13 @@
 #SBATCH --mail-type=BEGIN,END,FAIL
 
 input='/gpfs/space/GI/GV/EGCUT_data/omics_data/microbiomics/BGI_cleaned/'
-smpl1='lab64_VBBUAG_E100013123_L01/'
-smpl2='lab64_VBC2EG_E100013108_L01/'
-smpl3='lab64_VBCY6U_E100013421_L01/'
-
-NAME1=`echo "$smpl1" | cut -d'_' -f2`
-NAME2=`echo "$smpl2" | cut -d'_' -f2`
-NAME3=`echo "$smpl3" | cut -d'_' -f2`
-
 module load broadwell/megahit/1.2.9
 
-megahit -1 $input/$smpl1/filtered_nohost1.fq.gz -2 $input/$smpl1/filtered_nohost2.fq.gz -t 12 -o /gpfs/space/home/pantiukh/strain_het/assembly_$NAME1
-megahit -1 $input/$smpl2/filtered_nohost1.fq.gz -2 $input/$smpl2/filtered_nohost2.fq.gz -t 12 -o /gpfs/space/home/pantiukh/strain_het/assembly_$NAME2
-megahit -1 $input/$smpl3/filtered_nohost1.fq.gz -2 $input/$smpl3/filtered_nohost2.fq.gz -t 12 -o /gpfs/space/home/pantiukh/strain_het/assembly_$NAME3
+while read FQ; do
+
+  NAMEs=`echo "$FQ" | cut -d'_' -f2`
+  megahit -1 $input/$FQ/filtered_nohost1.fq.gz -2 $input/$FQ/filtered_nohost2.fq.gz -t 12 -o /gpfs/space/home/pantiukh/get_assembly/assembly_$NAMEs
+
+done </gpfs/space/home/pantiukh/get_assembly/contigs_list.csv
 
 module purge
